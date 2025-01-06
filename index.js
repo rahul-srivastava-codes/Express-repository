4e2427b7360a4b383a7dcb2e45534396f3514eb2
black-cat-developer <rahulsrichunar@gmail.com>
2025-01-06 18:19:24 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+var users = [
+  {
+    name: "John",
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
+app.get("/", (req, res) => {
+  const john_kidney = users[0].kidneys;
+  const numberofkidneys = john_kidney.length;
+  let numberofhealthykidneys = 0;
+  for (let i = 0; i < numberofkidneys; i++) {
+    if (john_kidney[i].healthy) {
+      numberofhealthykidneys++;
+    }
+  }
+  const numberofunhealthykidneys = numberofkidneys - numberofhealthykidneys;
+  res.json({
+    numberofkidneys,
+    numberofhealthykidneys,
+    numberofunhealthykidneys,
+  });
+});
+app.post("/", (req, res) => {
+  const ishealthy = req.body.ishealthy;
+  users[0].kidneys.push({
+    healthy: ishealthy,
+  });
+  res.json({
+    message: "kidney status updated",
+  });
+});
+
+app.put("/", (req, res) => {
+  for (let i = 0; i < users[0].length; i++) {
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
